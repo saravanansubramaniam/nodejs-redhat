0487631b6988e8f74440f3e0f43408c85e6445d7
saravanan.subramaniam <saravanan.subramaniam@ishafoundation.org>
2016-08-25 15:12:00 +0530

    port number changed

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -11,7 +11,7 @@ var sql = require('mssql');
 var app = express();
 
 // all environments
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
